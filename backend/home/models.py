6027e0172b548d07df018c8b85efd0e34ec16e98
Crowdbotics <engineering@crowdbotics.com>
2022-10-18 11:32:48 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Test(models.Model):
+    "Generated Model"
+    prd = models.BigIntegerField()
+    srd = models.TimeField()
