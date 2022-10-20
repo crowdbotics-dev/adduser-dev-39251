b371280a785e53f6329cca0bc242b2ed4ed4b978
Crowdbotics <engineering@crowdbotics.com>
2022-10-20 21:28:53 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Test(models.Model):
     "Generated Model"
     prd = models.BigIntegerField()
-    srd = models.TimeField()
 class Tested(models.Model):
     'Generated Model'
     newuser = models.UUIDField()
